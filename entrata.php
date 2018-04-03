c0178241aeb1c4f7f940a315cf1440c7f75b67ba
bmurillom <38001500+bmurillom@users.noreply.github.com>
2018-04-03 16:40:52 +0200

    Update entrata.php

--- entrata.php
+++ entrata.php
@@ -15,7 +15,7 @@ $ps=  $_POST['password'];
 
 $cn = mysqli_connect('mysql.hostinger.it', 'u426573260_luis', $_SESSION['pss'], 'u426573260_primo');
 
-$sql = "SELECT *  FROM Utente WHERE  nome = '$name' AND password = '$ps'";
+$sql = 'SELECT *  FROM Utente WHERE  nome = '$name' AND password = '$ps'';
 $ris = mysqli_query($cn,$sql) ; //esegue la query
 $n_ris = mysqli_num_rows($ris); //dice il numero di record trovati
 
