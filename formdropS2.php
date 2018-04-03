9feb8ab0dab99bca88be34f6927c8459eac3d89e
bmurillom <38001500+bmurillom@users.noreply.github.com>
2018-04-03 09:58:40 +0200

    Update formdropS2.php

--- formdropS2.php
+++ formdropS2.php
@@ -102,10 +102,8 @@ span.psw {
     <?php
      
 
-
+ $str='Lista sensori <br/> ';
 $codU = $_SESSION['id'] ;
-
-
 $cn = mysqli_connect('mysql.hostinger.it', 'u426573260_luis', 'cinegga00', 'u426573260_primo');
 $sql = 'SELECT *  FROM dispositivo ';
 $ris = mysqli_query($cn,$sql) ; //esegue la query
@@ -120,19 +118,19 @@ if ($n_ris> 0){
     while ($row = $ris->fetch_assoc()) {
     // output data of each row
     
-     echo " Id: ".$row['id']."  Tipo  ".$row['tipo']." Marca  ".$row['marca']."  Zona ".$row['zona ']."  Codice Utente  ".$row['proprieta']." ";
+     $str." Id: ".$row['id']."  Tipo  ".$row['tipo']." Marca  ".$row['marca']."  Zona ".$row['zona ']."  Codice Utente  ".$row['proprieta']." ";
     
        
        
     }
     
  }else {
-     echo" Non ci sono sensori";
+     echo'Non ci sono sensori';
 	 
 }
  
 
-   
+   echo"$str";
    
    
     
