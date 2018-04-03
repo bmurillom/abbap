6ae87547c714fb2467e9fc4a522c1585173e4c44
bmurillom <38001500+bmurillom@users.noreply.github.com>
2018-04-03 09:55:05 +0200

    Update tipoS.php

--- tipoS.php
+++ tipoS.php
@@ -15,15 +15,10 @@ if ($_SESSION['livello']>1 ){
 
 
 
-
+ $str='Lista tipi dei   sensori <br/> ';
 $codU = $_SESSION['id'] ;
-
-
 $cn = mysqli_connect('mysql.hostinger.it', 'u426573260_luis', 'cinegga00', 'u426573260_primo');
 $sql = 'SELECT  tipologia , primaP, secondaP, terzaP FROM  tipo ';
-        
-     
-        
 $ris = mysqli_query($cn,$sql) ; //esegue la query
 $n_ris = mysqli_num_rows($ris); //dice il numero di record trovati
 
@@ -33,22 +28,22 @@ $n_ris = mysqli_num_rows($ris); //dice il numero di record trovati
 
    
 if ($n_ris > 0){
-    echo "Lista tipi dei   sensori  ";
+   
    
     while ($row = $ris->fetch_assoc()) {
     // output data of each row
     
-    echo "Tipologia sensore  ".$row['tipologia']." Tipo valore stringa       ".$row['primaP']."   ".$row['secondaP']."  ".$row['terzaP']." ";
+    $str= $str."Tipologia sensore  ".$row['tipologia']." Tipo valore stringa       ".$row['primaP']."   ".$row['secondaP']."  ".$row['terzaP']." ";
      
     }
     
  }else {
-     echo" Non ci sono tipi ";
+     echo'Non ci sono tipi ';
 	 
 }
  
 
-   
+   echo"$str";
    
    
     
