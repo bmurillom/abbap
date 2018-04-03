24c27c3caa92b56790ba2e1e228989cd6d6c1edc
bmurillom <38001500+bmurillom@users.noreply.github.com>
2018-04-03 10:08:48 +0200

    Update sensoreall.php

--- sensoreall.php
+++ sensoreall.php
@@ -9,11 +9,9 @@ session_start();
 
      
 
-
+$str='Lista sensori <br/>';
 $codU = $_SESSION['id'] ;
-
-	$cn = mysqli_connect('mysql.hostinger.it', 'u426573260_luis', 'cinegga00', 'u426573260_primo');
-
+$cn = mysqli_connect('mysql.hostinger.it', 'u426573260_luis', 'cinegga00', 'u426573260_primo');
 $sql = "SELECT *  FROM dispositivo where proprieta = '$codU' ";
 $ris = mysqli_query($cn,$sql) ; //esegue la query
 $n_ris = mysqli_num_rows($ris); //dice il numero di record trovati
@@ -27,7 +25,7 @@ if ($n_ris> 0){
     while ($row = $ris->fetch_assoc()) {
     // output data of each row
  
- echo "Id ".$row['id']."   Tipo  ".$row['tipo']."  Marca  ".$row['marca']."   Zona ".$row['zona']."  Codice Utente  ".$row['proprieta']." ";
+ $str= $str."Id ".$row['id']."   Tipo  ".$row['tipo']."  Marca  ".$row['marca']."   Zona ".$row['zona']."  Codice Utente  ".$row['proprieta']." ";
     
        
        
@@ -39,7 +37,7 @@ if ($n_ris> 0){
 }
  
 
-   
+   echo"$str";
    
    
     
