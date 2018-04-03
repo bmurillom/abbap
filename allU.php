306898ac49f99bf38773fbbb26fb0f0fe2e22aa7
bmurillom <38001500+bmurillom@users.noreply.github.com>
2018-04-03 10:32:11 +0200

    Update allU.php

--- allU.php
+++ allU.php
@@ -22,7 +22,8 @@ if ($_SESSION['livello']>1 ){
 
 
 $str= 'Utenti :<br/>';
-$cn = mysqli_connect('mysql.hostinger.it', 'u426573260_luis', 'cinegga00', 'u426573260_primo');
+$ps= 'cinegga00'
+$cn = mysqli_connect('mysql.hostinger.it', 'u426573260_luis',$ps , 'u426573260_primo');
 $sql = 'SELECT *  FROM Utente ';
 $ris = mysqli_query($cn,$sql) ; //esegue la query
 $n_ris = mysqli_num_rows($ris); //dice il numero di record trovati
